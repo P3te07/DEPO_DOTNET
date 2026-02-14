081a2a22df2ba4e3f9a94f95a685a4acda42311a
P3te07 <petruodobescu@gmail.com>
2026-02-14 19:46:01 +0200

    Testare auth

---
+++ Views/Shared/_Navigation.cshtml.cs
@@ -0,0 +1,12 @@
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Mvc.RazorPages;
+
+namespace Proiect_ASPDOTNET.Views.Shared
+{
+    public class _NavigationModel : PageModel
+    {
+        public void OnGet()
+        {
+        }
+    }
+}
